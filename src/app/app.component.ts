430d545a66b81517bb42b30509d2fa6bfeb5e9bc
APA\SESA489434 <deep.hazra@non.schneider-electric.com>
2019-01-19 20:22:45 +0530

    two more tables added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,24 @@ import {SafeTrust} from './shared/models/safe-trust';
 
 
 const ABCP_TABLE_1: SafeTrust[] = [
+  {name: 'Safe Trust Series 1996-1', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1', date: '(Nov 30 2018)', action: 'Archive'}
+];
+const ABCP_TABLE_2: SafeTrust[] = [
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 French', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 French', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 French', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 French', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'},
+  {name: 'Safe Trust Series 1996-1 English', date: '(Dec 10 2015)', action: 'Archive'}
+];
+const ABCP_TABLE_3: SafeTrust[] = [
   {name: 'Hydrogen', date: '(Nov 30 2018)', action: 'Archive'},
   {name: 'Helium', date: '(Nov 30 2018)', action: 'Archive'},
   {name: 'Lithium', date: '(Nov 30 2018)', action: 'Archive'},
@@ -17,5 +35,9 @@ const ABCP_TABLE_1: SafeTrust[] = [
 
 export class AppComponent {
   abcpTable1 = ABCP_TABLE_1;
+  abcpTable2 = ABCP_TABLE_2;
+  abcpTable3 = ABCP_TABLE_3;
   abcpTableTitle1 = 'CIBC-sponsored Asset-Backed Commercial Paper Conduits';
+  abcpTableTitle2 = 'Information Memorandum';
+  abcpTableTitle3 = 'Conduits Fact Sheet';
 }
