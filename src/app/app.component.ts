c03117e2b228e2fa54a96e7b4ab8b7b560d77cfe
APA\SESA489434 <deep.hazra@non.schneider-electric.com>
2019-01-19 19:44:40 +0530

    Abcp Table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SafeTrust} from './shared/models/safe-trust';
+
+
+const ABCP_TABLE_1: SafeTrust[] = [
+  {name: 'Hydrogen', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Helium', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Lithium', date: '(Nov 30 2018)', action: 'Archive'},
+  {name: 'Beryllium', date: '(Nov 30 2018)', action: 'Archive'}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Landing';
+  abcpTable1 = ABCP_TABLE_1;
+  abcpTableTitle1 = 'CIBC-sponsored Asset-Backed Commercial Paper Conduits';
 }
---
+++ src/app/shared/models/safe-trust.ts
@@ -0,0 +1,5 @@
+export interface SafeTrust {
+  name: string;
+  date: string;
+  action: string;
+}
